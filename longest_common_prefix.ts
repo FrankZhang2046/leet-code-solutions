c617d2eb6162e0627c132c00faba8c73d28a0a81
Frank Zhang <frankbusinessmail@gmail.com>
2022-11-10 22:28:23 -0500

    .gitignore file added

---
+++ longest_common_prefix.ts
@@ -0,0 +1,19 @@
+function longestCommonPrefix(strs): string {
+  let returnString = "";
+  let currentLetter: string;
+  for (let index = 0; index < strs[0].length; index++) {
+    currentLetter = strs[0][index];
+    let existInAllWords = true;
+    strs.filter((word, index) => index != 0).forEach(word => {
+      if (!word[index] || word[index] !== currentLetter) {
+        existInAllWords = false;
+      }     
+    })
+    if (existInAllWords) {
+      returnString += currentLetter;
+    }
+  }
+  return returnString;
+}
+
+console.log(longestCommonPrefix(["dog","racecar","car"]));
