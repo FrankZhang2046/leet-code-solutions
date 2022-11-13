8806391e5f947fe8d049538b778192acc816b33f
Frank Zhang <frankbusinessmail@gmail.com>
2022-11-12 20:56:09 -0500

    working on latest problem

---
+++ src/merge_two_sorted_lists.ts
@@ -0,0 +1,14 @@
+ // Definition for singly-linked list.
+ class ListNode {
+   val: number;
+   next: ListNode | null;
+   constructor(val?: number, next?: ListNode | null) {
+     this.val = val === undefined ? 0 : val;
+     this.next = next === undefined ? null : next;
+   }
+ }
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    console.log(list1, list2);
+    return list2;
+};
--- src/valid_parentheses.ts
+++ src/valid_parentheses.ts
@@ -1,26 +1,35 @@
-function isValid(s:string): boolean {
+function isValid(s: string): boolean {
   let symbolsStack = [];
   for (let index = 0; index < s.length; index++) {
-    if (s[index] === '(' || s[index] === '{' || s[index] === '[') {
+    if (s[index] === "(" || s[index] === "{" || s[index] === "[") {
       symbolsStack.push(s[index]);
     } else {
       switch (s[index]) {
-        case '}':
-          if (symbolsStack[symbolsStack.length - 1] && symbolsStack[symbolsStack.length - 1] === '{') {
+        case "}":
+          if (
+            symbolsStack[symbolsStack.length - 1] &&
+            symbolsStack[symbolsStack.length - 1] === "{"
+          ) {
             symbolsStack.pop();
           } else {
             return false;
           }
           break;
-        case ')':
-          if (symbolsStack[symbolsStack.length - 1]&& symbolsStack[symbolsStack.length - 1] === '(') {
+        case ")":
+          if (
+            symbolsStack[symbolsStack.length - 1] &&
+            symbolsStack[symbolsStack.length - 1] === "("
+          ) {
             symbolsStack.pop();
           } else {
             return false;
           }
           break;
-        case ']':
-          if (symbolsStack[symbolsStack.length - 1] && symbolsStack[symbolsStack.length - 1] === '[') {
+        case "]":
+          if (
+            symbolsStack[symbolsStack.length - 1] &&
+            symbolsStack[symbolsStack.length - 1] === "["
+          ) {
             symbolsStack.pop();
           } else {
             return false;
