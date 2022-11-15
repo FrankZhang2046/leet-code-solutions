d8e7075f4f0d9af1fc12795980ceb79a1337adfc
Frank Zhang <frank.zhang@ratio.city>
2022-11-14 21:25:37 -0500

    Revert "made some progress"
    This reverts commit 53d3ed7fdbdf9ad447f59e390c5fdc863c328100.

--- src/merge_two_sorted_lists.ts
+++ src/merge_two_sorted_lists.ts
@@ -8,49 +8,6 @@
    }
  }
 
-const firstNode = new ListNode(1);
-const secondNode = new ListNode(2, firstNode);
-const thirdNode = new ListNode(3, secondNode);
-const fourthNode = new ListNode(4, thirdNode);
-const fifthNode = new ListNode(5, fourthNode);
-
-const sampleData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// function that converts an array of integers into a linked list
-function loadList(intArr: number[]): ListNode {
-  let prevNode: ListNode;
-  intArr.forEach(( integer, index ) => {
-    let newNode: ListNode;
-    if (index === 0) {
-      newNode = new ListNode(integer);
-    } else {
-      newNode = new ListNode(integer, prevNode);
-    }
-    prevNode = newNode;
-  });
-  return prevNode;
-}
-
-
-// function that traverses through a linked list and prints the values of each node
-function traverseList(listNode: ListNode): void {
-  let currentNode = listNode;
-  while (currentNode.val) {
-    console.log(currentNode.val);
-    if (currentNode.next) {
-      currentNode = currentNode.next;
-    } else {
-      break;
-    }
-  }
-}
-
-const lastNode = loadList(sampleData);
-traverseList(lastNode)
-
-const firstSideNode = new ListNode(1);
-const secondSideNode = new ListNode(3, firstSideNode);
-const thirdSideNode = new ListNode(4, firstSideNode);
-
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
     console.log(list1, list2);
     return list2;
