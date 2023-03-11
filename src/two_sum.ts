445289127d382357a8ddec4efe4e55d78d638d15
Frank Zhang <frankbusinessmail@gmail.com>
2023-03-10 22:03:05 -0500

    did some studying today

--- src/two_sum.ts
+++ src/two_sum.ts
@@ -1,3 +1,4 @@
+/*
 function twoSum(numbers: number[], target: number): number[] {
     let start = 0, end = numbers.length - 1;
     while ((numbers[start] + numbers[end]) !== target) {
@@ -9,4 +10,24 @@ function twoSum(numbers: number[], target: number): number[] {
     }
 
     return [start+1, end+1];
+};
+
+*/
+
+// * alternative solution
+function twoSum(nums: number[], target: number): number[] {
+    const myMap = new Map<number, number>();
+    
+    function recursiveHelper(currIdx: number): number[] {
+        const targetVal = target - nums[currIdx];
+
+        if (myMap.has(targetVal)) {
+            return [currIdx, myMap.get(targetVal)];
+        }
+
+        myMap.set(nums[currIdx], currIdx);
+        return recursiveHelper(currIdx + 1);
+    }
+
+    return recursiveHelper(0);
 };
