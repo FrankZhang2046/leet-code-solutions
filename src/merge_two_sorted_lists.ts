f7101da9a51a79f3343f6fdb802de59ff9c0bcab
Frank Zhang <frankbusinessmail@gmail.com>
2023-03-12 10:54:23 -0400

    updated merge two lists solution with more elegant code

--- src/merge_two_sorted_lists.ts
+++ src/merge_two_sorted_lists.ts
@@ -1,46 +1,26 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+  class ListNode {
+      val: number
+      next: ListNode | null
+      constructor(val?: number, next?: ListNode | null) {
+          this.val = (val===undefined ? 0 : val)
+          this.next = (next===undefined ? null : next)
+      }
+  }
 
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    const head = new ListNode(null);
-    function recursiveHelper(l1, l2, pointer) {
-        if (!l1 && !l2) {
-            return; // terminal case
-        } else if (!l1) {
-            pointer.next = l2;
-            return;
-        } else if (!l2) {
-            pointer.next = l1;
-            return;
-        }
+    let head: ListNode | null; 
+    if (!list1) {return list2;}
+    if (!list2) {return list1;}
 
-        if (l1.val === l2.val) {
-            pointer.next = new ListNode(l1.val, null);
-            pointer = pointer.next;
-            pointer.next = new ListNode(l2.val, null);
-            pointer = pointer.next;
-            recursiveHelper(l1.next, l2.next, pointer)
-        } else if (l1.val < l2.val) {
-            pointer.next = new ListNode(l1.val, null);
-            pointer= pointer.next;
-            recursiveHelper(l1.next, l2, pointer);
-        } else if (l1.val > l2.val) {
-            pointer.next = new ListNode(l2.val, null);
-            pointer= pointer.next;
-            recursiveHelper(l1, l2.next, pointer);
-        }
-        
+    if (list1.val < list2.val) {
+        head = list1;
+        list1 = list1.next;
+    } else {
+        head = list2;
+        list2=list2.next;
     }
+    
+    head.next = mergeTwoLists(list1, list2);
 
-    recursiveHelper(list1, list2, head);
-    return head.next;
-};
+    return head;
+}
