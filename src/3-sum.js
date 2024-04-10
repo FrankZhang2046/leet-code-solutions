01194b0e58533c379eb6db4f7e288698da5c156b
Frank Zhang <frankbusinessmail@gmail.com>
2024-04-10 16:55:20 -0400

    added two pointer solution for 3sum

--- src/3-sum.js
+++ src/3-sum.js
@@ -23,3 +23,54 @@ var threeSum = function (nums) {
 
     return returnArr;
 };
+var threeSumTwoPointer = function (nums) {
+    /*
+        - sort the array to avoid duplicates
+        - iterate through the array, avoid duplicates
+            - 0 - nums[i] becomes the new target number
+                - use two pointers methods
+                - left pointer start at i+1, right pointer start at nums[nums.length-1]
+                    - if lp + rp === target, new solution found.   *
+                    - if lp + rp < target, increment lp, avoid dups
+                    - if lp + rp > target, decrement rp, avoid dups
+    */
+    const returnArr = [];
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        } else {
+            const target = 0 - nums[i];
+
+            let lp = i + 1;
+            let rp = nums.length - 1;
+
+            while (lp < rp) {
+                const sum = nums[lp] + nums[rp];
+                if (sum === target) {
+                    // found a new solution
+                    returnArr.push([nums[i], nums[lp], nums[rp]]);
+                    while (nums[lp] === nums[lp + 1]) {
+                        lp++
+                    };
+                    while (nums[rp] === nums[rp - 1]) {
+                        rp--
+                    };
+                    lp++
+                    rp--;
+                }
+                if (sum > target) {
+                    rp--;
+                }
+
+                if (sum < target) {
+                    lp++;
+                }
+            }
+        }
+
+    }
+
+
+    return returnArr;
+};
